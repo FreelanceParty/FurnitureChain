fc2161931e3ce043a0f253dce6f1b8591a14718a
Viacheslav Blazhkov <viacheslav.b@droam.com>
2025-04-01 23:25:48 +0300

    [#25] Created basic button

--- resources/views/_elements/button.blade.php
+++ resources/views/_elements/button.blade.php
@@ -1,3 +1,12 @@
-<div id="{{ $id }}" class="flex h-fit w-fit p-2 rounded-sm border bg-gray-200 cursor-pointer {{ $class ?? '' }}">
+<button id="{{ $id }}"
+		type="submit"
+		class="disabled:bg-gray-300 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 {{ $class ?? '' }}"
+		@if( $isDisabled ?? FALSE )
+			disabled
+		@endif
+		@if( ! empty($routeName) )
+			data-route="{{ $routeName }}"
+		@endif
+>
 	{{ $text }}
-</div>
+</button>
